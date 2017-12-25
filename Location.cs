716c28372bbab1228a91ac1ede2c9b86c531da52
Ryan Nelson <ryan.rnelson30@gmail.com>
2017-12-25 15:26:45 -0500

    Update Location.cs

--- Location.cs
+++ Location.cs
@@ -7,14 +7,13 @@ namespace GSios.Models
 {
     public class Location
     {
-		[JsonProperty(PropertyName = "Id")]
-		public string Id { get; set; }
+	[JsonProperty(PropertyName = "Id")]
+	public string Id { get; set; }
 
         [JsonProperty(PropertyName = "Coords")]
         public CLLocation Coords { get; set; }
 
-		[Version]
-		public string Version { get; set; }
-
+	[Version]
+	public string Version { get; set; }
     }
 }
