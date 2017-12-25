88fce1f066a0a160483b602bf3a6208a4ab1b68c
Ryan Nelson <ryan.rnelson30@gmail.com>
2017-12-25 14:38:09 -0500

    Update LocationTableViewController.cs

--- LocationTableViewController.cs
+++ LocationTableViewController.cs
@@ -11,152 +11,150 @@ using System.Threading.Tasks;
 namespace GSios
 {
     public partial class LocationTableViewController : UITableViewController
-	{
-		private string pathToDatabase;
+    {
+        private string pathToDatabase;
         static readonly string mapItemCellId = "mapItemCellId2";
         public MKMapView Map { get; set; }
-		public List<SavedLocation> SavedLocations { get; set; }
+	public List<SavedLocation> SavedLocations { get; set; }
         public UIView ContainerView { get; set; }
 
-		public LocationTableViewController(IntPtr handle) :base(handle)
+	public LocationTableViewController(IntPtr handle) :base(handle)
         {
-			SavedLocations = new List<SavedLocation>();
-		}
+		SavedLocations = new List<SavedLocation>();
+	}
 
-		public override void ViewDidLoad()
+	public override void ViewDidLoad()
+	{
+		base.ViewDidLoad();
+
+		var documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+		pathToDatabase = Path.Combine(documentsFolder, "locations_db.db");
+
+		using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
 		{
-			base.ViewDidLoad();
-
-			var documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
-			pathToDatabase = Path.Combine(documentsFolder, "locations_db.db");
-
-			using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
-			{
-				connection.CreateTable<SavedLocation>();
-			}
-            RefreshControl = new UIRefreshControl();
-			RefreshControl.ValueChanged += async (sender, e) =>
-			{
-				await RefreshAsync();
-			};
+			connection.CreateTable<SavedLocation>();
 		}
-
-		public override void ViewDidAppear(bool animated)
+                RefreshControl = new UIRefreshControl();
+		RefreshControl.ValueChanged += async (sender, e) =>
 		{
-			base.ViewDidAppear(animated);
-			SavedLocations = new List<SavedLocation>();
+			await RefreshAsync();
+		};
+	}
 
-			using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
+	public override void ViewDidAppear(bool animated)
+	{
+		base.ViewDidAppear(animated);
+		SavedLocations = new List<SavedLocation>();
+
+		using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
+		{
+			var query = connection.Table<SavedLocation>();
+
+			foreach (SavedLocation mapItem in query)
 			{
-				var query = connection.Table<SavedLocation>();
-
-				foreach (SavedLocation mapItem in query)
-				{
-					SavedLocations.Add(mapItem);
-					TableView.ReloadData();
-				}
-
+				SavedLocations.Add(mapItem);
+				TableView.ReloadData();
 			}
+
 		}
+	}
 
         private async Task RefreshAsync(){
 			
-            RefreshControl.BeginRefreshing();
-			SavedLocations = new List<SavedLocation>();
+            	RefreshControl.BeginRefreshing();
+		SavedLocations = new List<SavedLocation>();
 
+		using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
+		{
+			var query = connection.Table<SavedLocation>();
+
+			foreach (SavedLocation mapItem in query)
+			{	
+				SavedLocations.Add(mapItem);
+				TableView.ReloadData();
+			}
+
+		}
+		RefreshControl.EndRefreshing();
+		TableView.ReloadData();
+        }
+	public override nint RowsInSection(UITableView tableView, nint section)
+	{
+		return SavedLocations.Count;
+	}
+
+	public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+	{
+		UITableViewCell cell = tableView.DequeueReusableCell(mapItemCellId);
+
+		if (cell == null)
+			cell = new UITableViewCell();
+
+		var data = SavedLocations[indexPath.Row];
+
+		cell.TextLabel.Text = data.Name;
+         	return cell;
+	}
+	public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
+	{
+		switch (editingStyle)
+		{
+			case UITableViewCellEditingStyle.Delete:
 			using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
 			{
-				var query = connection.Table<SavedLocation>();
-
-				foreach (SavedLocation mapItem in query)
-				{
-					SavedLocations.Add(mapItem);
-					TableView.ReloadData();
-				}
-
+				connection.Delete(SavedLocations[indexPath.Row]);
 			}
-			RefreshControl.EndRefreshing();
-
-			TableView.ReloadData();
-        }
-		public override nint RowsInSection(UITableView tableView, nint section)
-		{
-			return SavedLocations.Count;
-		}
-
-		public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
-		{
-			UITableViewCell cell = tableView.DequeueReusableCell(mapItemCellId);
-
-			if (cell == null)
-				cell = new UITableViewCell();
-
-			var data = SavedLocations[indexPath.Row];
-
-			cell.TextLabel.Text = data.Name;
-
-			return cell;
-		}
-		public override void CommitEditingStyle(UITableView tableView, UITableViewCellEditingStyle editingStyle, Foundation.NSIndexPath indexPath)
-		{
-			switch (editingStyle)
-			{
-				case UITableViewCellEditingStyle.Delete:
-					using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
-					{
-						connection.Delete(SavedLocations[indexPath.Row]);
-					}
-                    var annotations = Map.Annotations;
-                    foreach(IMKAnnotation pin in annotations){
+                    	var annotations = Map.Annotations;
+                    	foreach(IMKAnnotation pin in annotations){
                         if(pin.Coordinate.Latitude == SavedLocations[indexPath.Row].Latitude && 
-                           pin.Coordinate.Longitude == SavedLocations[indexPath.Row].Longitude){
-                            Map.RemoveAnnotation(pin);
+                        	pin.Coordinate.Longitude == SavedLocations[indexPath.Row].Longitude){
+                            	Map.RemoveAnnotation(pin);
                         }
-                    }
-					// remove the item from the underlying data source
-					SavedLocations.RemoveAt(indexPath.Row);
-                    // delete the row from the table
-                    tableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
-					break;
-				case UITableViewCellEditingStyle.None:
-					Console.WriteLine("CommitEditingStyle:None called");
-					break;
-			}
+       		}
+		// remove the item from the underlying data source
+		SavedLocations.RemoveAt(indexPath.Row);
+                // delete the row from the table
+                tableView.DeleteRows(new NSIndexPath[] { indexPath }, UITableViewRowAnimation.Fade);
+			break;
+		case UITableViewCellEditingStyle.None:
+		Console.WriteLine("CommitEditingStyle:None called");
+			break;
 		}
-		public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
-		{
-			return true; // return false if you wish to disable editing for a specific indexPath or for all rows
-		}
-		public override string TitleForDeleteConfirmation(UITableView tableView, NSIndexPath indexPath)
-		{   // Optional - default text is 'Delete'
-			return "Delete";
-		}
+	}
+	public override bool CanEditRow(UITableView tableView, NSIndexPath indexPath)
+	{
+		return true; // return false if you wish to disable editing for a specific indexPath or for all rows
+	}
+		
+	public override string TitleForDeleteConfirmation(UITableView tableView, NSIndexPath indexPath)
+	{   // Optional - default text is 'Delete'
+		return "Delete";
+	}
+	
+	public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
+	{
+		// add item to map
+        	var mapItem = SavedLocations[indexPath.Row];
+            	CLLocationCoordinate2D coord = new CLLocationCoordinate2D(mapItem.Latitude, mapItem.Longitude);
 
-		public override void RowSelected(UITableView tableView, NSIndexPath indexPath)
-		{
-			// add item to map
-			
-            var mapItem = SavedLocations[indexPath.Row];
-            CLLocationCoordinate2D coord = new CLLocationCoordinate2D(mapItem.Latitude, mapItem.Longitude);
+		Map.SetCenterCoordinate(coord, true);
+            	ContainerView.Hidden = true;
+	}
 
-			Map.SetCenterCoordinate(coord, true);
-            ContainerView.Hidden = true;
-		}
-
-        private void RefreshMap(){
-            
+        private void RefreshMap(){    
         }
     }
-	public class SavedLocation
-	{
+    
+    public class SavedLocation
+    {
 
-		[PrimaryKey, AutoIncrement]
-		public int Id { get; set; }
+	[PrimaryKey, AutoIncrement]
+	public int Id { get; set; }
 
         public string Name { get; set; }
 
         public double Longitude { get; set; }
 
         public double Latitude { get; set; }
-	}
+    }
 }
