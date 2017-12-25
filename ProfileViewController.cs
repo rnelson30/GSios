e616a4353210603ff3653d6af08b07006a0f8b51
Ryan Nelson <ryan.rnelson30@gmail.com>
2017-12-25 15:09:47 -0500

    Update ProfileViewController.cs

--- ProfileViewController.cs
+++ ProfileViewController.cs
@@ -10,10 +10,11 @@ namespace GSios
 {
     public partial class ProfileViewController : UIViewController
     {
-		private string pathToDatabase;
-		private string documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+	private string pathToDatabase;
+	private string documentsFolder = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
         private string ProfileName;
-		public ProfileViewController(IntPtr handle) : base(handle)
+	
+	public ProfileViewController(IntPtr handle) : base(handle)
         {
         }
         public override void ViewDidLoad()
@@ -25,26 +26,24 @@ namespace GSios
             ProfileLabel.TextAlignment = UITextAlignment.Center;
 
         }
-        private void SetUserName(){
+        private void SetUserName()
+	{
             pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
             using (var connection = new SQLiteConnection(pathToDatabase))
             {
-				var user = connection.Table<LocalUser>().Where(us => us.LoggedIn == true);
+	        var user = connection.Table<LocalUser>().Where(us => us.LoggedIn == true);
                 ProfileName = user.ElementAt(0).Name; 
-			}
+	    }
         }
         partial void SignOutButton_TouchUpInside(UIButton sender)
         {
             pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
-			using (var connection = new SQLiteConnection(pathToDatabase))
-			{
+	    using (var connection = new SQLiteConnection(pathToDatabase))
+	    {
                 var user = connection.Table<LocalUser>().Where(us => us.LoggedIn == true);
                 var i = connection.Execute("update LocalUser set LoggedIn = 'false' where Id = @id",user.ElementAt(0).Id);
-
-			}
+	    }
             DismissViewController(true,null);
         }
     }
-	
-	
 }
