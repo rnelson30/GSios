c6952ce4483d39873bf5e56c9cdbd4dce9a8cb61
Ryan Nelson <ryan.rnelson30@gmail.com>
2017-12-25 15:25:55 -0500

    Update CustomAnnotation.cs

--- CustomAnnotation.cs
+++ CustomAnnotation.cs
@@ -6,42 +6,40 @@ namespace GSios.Models
 {
     public class CustomAnnotation : MKAnnotation
     {
-		string title;
-		CLLocationCoordinate2D coord;
+	string title;
+	CLLocationCoordinate2D coord;
         int enclosedMembers;
 
-		public CustomAnnotation(string title, CLLocationCoordinate2D coord, int enclosedMembers)
-		{
-			this.title = title;
-			this.coord = coord;
+	public CustomAnnotation(string title, CLLocationCoordinate2D coord, int enclosedMembers)
+	{
+	    this.title = title;
+	    this.coord = coord;
             this.enclosedMembers = enclosedMembers;
 
-		}
+	}
 
-		public override string Title
-		{
-			get
-			{
-				return title;
-			}
-		}
+	public override string Title
+	{
+	    get
+	    {
+		return title;
+	    }
+	}
 
-		public override CLLocationCoordinate2D Coordinate
-		{
-			get
-			{
-				return coord;
-			}
-		}
+	public override CLLocationCoordinate2D Coordinate
+	{
+	    get
+	    {
+		return coord;
+	    }
+	}
 
         public int EnclosedMembers
         {
             get 
             {
                 return enclosedMembers;
-            }
-            
+            }   
         }
-
     }
 }
