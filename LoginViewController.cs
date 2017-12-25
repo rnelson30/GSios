6458963d2660d24195eadb67cd21101803072745
Ryan Nelson <ryan.rnelson30@gmail.com>
2017-12-25 15:00:31 -0500

    Update LoginViewController.cs

--- LoginViewController.cs
+++ LoginViewController.cs
@@ -21,89 +21,83 @@ namespace GSios
             localUsers = new List<LocalUser>(); 
         }
         
-		public override void ViewDidLoad()
-		{
-			base.ViewDidLoad();
+	public override void ViewDidLoad()
+	{
+	    base.ViewDidLoad();
             View.BackgroundColor = UIColor.Black;
 
             //local User DB
-			pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
+	    pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
 
-			using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
-			{
-				connection.CreateTable<LocalUser>();
-                //connection.DeleteAll<LocalUser>(); clear local user DB
-			}
-		}
-		public override void ViewDidAppear(bool animated)
-		{
-			base.ViewDidAppear(animated);
-
+	    using (var connection = new SQLite.SQLiteConnection(pathToDatabase))
+	    {
+	       connection.CreateTable<LocalUser>();
+               //connection.DeleteAll<LocalUser>(); clear local user DB
+	    }
+	}
+	public override void ViewDidAppear(bool animated)
+	{
+	    base.ViewDidAppear(animated);
             pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
             int count = 0;
             using (var connection = new SQLiteConnection(pathToDatabase))
             {
                var query = connection.Table<LocalUser>().Where(us => us.LoggedIn == true);
                count = query.Count();
-
             }
             if (count != 0)
-            {
-                LoginButton.SendActionForControlEvents(UIControlEvent.TouchUpInside);
-            }
-            else{
-                LoginToFaceBook();
-            }
-		}
+               LoginButton.SendActionForControlEvents(UIControlEvent.TouchUpInside);
+            else
+               LoginToFaceBook();
+	}
 
         //Facebook Login
-		private void LoginToFaceBook()
-		{
-			var auth = new OAuth2Authenticator(
-				 clientId: "************", //ID hidden 
-				 scope: "",
-				 authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
-				 redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html"));
-			auth.Completed += Auth_Completed;
-			var ui = auth.GetUI();
-			PresentViewController(ui, true, null);
-		}
+	private void LoginToFaceBook()
+	{
+	    var auth = new OAuth2Authenticator(
+	       clientId: "************", //ID hidden 
+	       scope: "",
+	       authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
+	       redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html"));
+	       auth.Completed += Auth_Completed;
+	       var ui = auth.GetUI();
+	       PresentViewController(ui, true, null);
+	}
 
-		private async void Auth_Completed(object sender, AuthenticatorCompletedEventArgs e)
-		{
-			if (e.IsAuthenticated)
-			{
-				var request = new OAuth2Request(
-					"GET",
-					new Uri("https://graph.facebook.com/me?fields=name"),
-					null,
-					e.Account);
+  	private async void Auth_Completed(object sender, AuthenticatorCompletedEventArgs e)
+	{
+	    if (e.IsAuthenticated)
+	    {
+	       var request = new OAuth2Request(
+	          "GET",
+		  new Uri("https://graph.facebook.com/me?fields=name"),
+		  null,
+		  e.Account);
 
-				var fbResponse = await request.GetResponseAsync();
-				var fbUser = JsonValue.Parse(fbResponse.GetResponseText());
+	       var fbResponse = await request.GetResponseAsync();
+	       var fbUser = JsonValue.Parse(fbResponse.GetResponseText());
 
-				var name = fbUser["name"];
-				var id = fbUser["id"];
+	       var name = fbUser["name"];
+	       var id = fbUser["id"];
 
-                if(!(IsExistingUser(id)))
-				    SaveUserLocally(id,name);
-                    AddUserToDB(id,name);
-			}
-
-			DismissViewController(true, null);
-
+               if(!(IsExistingUser(id)))
+	          SaveUserLocally(id,name);
+               AddUserToDB(id,name);
+	    }
+	    
+	    DismissViewController(true, null);
         }
         private bool IsExistingUser(long id)
         {
-			pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
+	    pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
             int count = 0;
-			using (var connection = new SQLiteConnection(pathToDatabase))
-			{
-                var user = connection.Table<LocalUser>().Where(u => u.UserId == id);
-                count = user.Count();
-                if(count !=0){
+	    using (var connection = new SQLiteConnection(pathToDatabase))
+	    {
+               var user = connection.Table<LocalUser>().Where(u => u.UserId == id);
+               count = user.Count();
+               if(count !=0){
                   var i =  connection.Execute("update LocalUser set LoggedIn = @val where Id = @id", true, user.ElementAt(0).Id);
-                }
+               }
             }
             if (count == 0)
                 return false;
@@ -115,7 +109,7 @@ namespace GSios
         }
 
         private void SaveUserLocally(long id, string name){
-			pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
+	    pathToDatabase = Path.Combine(documentsFolder, "users_db.db");
 
             using (var connection = new SQLiteConnection(pathToDatabase))
             {
@@ -127,18 +121,18 @@ namespace GSios
                 });
             }
         }
-    }
+     }
 
-	public class LocalUser
-	{
-		[PrimaryKey, AutoIncrement]
-		public int Id { get; set; }
+     public class LocalUser
+     {
+        [PrimaryKey, AutoIncrement]
+	public int Id { get; set; }
 
         public long UserId { get; set; }
 
-		public string Name { get; set; }
+	public string Name { get; set; }
 
         public bool LoggedIn { get; set; }
 
-	}
+     }
 }
